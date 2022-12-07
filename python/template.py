a50464a94bb30f299a98ee09c8c5b9c7d22e3eb8
charlie <charlieJ107@outlook.com>
2022-12-07 10:02:59 +0000

    Add Ex10

--- python/template.py
+++ python/template.py
@@ -226,107 +226,158 @@ def exercise6_rec(l):
     else:
         return table + 1
 
+
 def exercise6(l):
     current_depth = 0
     max_depth = 0
     for i in str(l):
         if i == '[':
             # list start
-            current_depth+=1
+            current_depth += 1
         elif i == ']':
             # list end
             if current_depth > max_depth:
                 max_depth = current_depth
             current_depth -= 1
     return max_depth
-        
-        
+
 
 # Exercise 7 - Change, please
 
 
 def exercise7(amount, coins):
-    # TODO Debug
-    possible_coins = (200, 100, 50, 20, 10, 5, 2, 1)
+    possible_coins = (1, 2, 5, 10, 20, 50, 100, 200)
     amount = int(amount * 100)
     # The worse idea, use 1p coin only
-    coins_needed = [int(amount/0.01) for _ in range(int(amount/0.01) + 1)]
-    for still_need_amount in range(len(coins_needed)):
-        for this_kind_of_coin in possible_coins:
-            if amount - this_kind_of_coin > 0:
-                # if amount - current_coin < 0, this coin is too much for this amount
-                
-                # we still need one more coin to have this amount
-                coins_needed[still_need_amount] = min(
-                    coins_needed[still_need_amount - this_kind_of_coin] + 1, # at this amount of changes, use this kind of coin will be better (less use of coins)
-                    coins_needed[still_need_amount] # or other kind of coins is good enough
-                    )
-    return coins_needed[amount] > coins
+    coins_needed = [amount for _ in range(amount + 1)]
+    coins_needed[0] = 0
+    for amount_still_needed in range(1, amount + 1):
+        for value_of_this_coin in possible_coins:
+            # if amount_still_needed - value_of_this_coin < 0, this coin is too much for this amount
+            if value_of_this_coin <= amount_still_needed:
+                # coins you need if you have one more this coin < coins you need if you have one more 1p 
+                if coins_needed[amount_still_needed - value_of_this_coin] < coins_needed[amount_still_needed] + 1:
+                    # Number of coins you need for amount_still_needed can be updated as one more this coin + number of coins you need for amount that without this coin
+                    coins_needed[amount_still_needed] = coins_needed[amount_still_needed - value_of_this_coin] + 1
+    return coins_needed[amount] <= coins
+
 
 # Exercise 8 - Five Letter Unscramble
 
 
 def exercise8(s):
-    origin = s # backup s since we will pop founded characters in s
+    origin = s  # backup s since we will pop founded characters in s
     count = 0
     with open("test_data/wordle.txt", 'r', encoding="utf-8") as f:
-        for i in f.readlines(): # i: each word in wordle set
+        for i in f.readlines():  # i: each word in wordle set
             i = i.replace("\n", "")
             all_c_in_i_is_in_s = False
-            for c in i: # c: each character in a word from wordle set
+            for c in i:  # c: each character in a word from wordle set
                 # O(n) if len(i) is always 5, aka. length of every word in wordle set is 5
                 if c in s:
                     all_c_in_i_is_in_s = True
-                    s = s.replace(c, "", 1) # pop founded characters from s
-                else: 
+                    s = s.replace(c, "", 1)  # pop founded characters from s
+                else:
                     all_c_in_i_is_in_s = False
                     break
             if all_c_in_i_is_in_s:
                 count += 1
-            s = origin # restore s from backup
+            s = origin  # restore s from backup
     return count
 
-# Exercise 9 - Wordle Set
+# Helper function for ex9 and ex10
 
-def exercise9(green, yellow, gray):
+# time: O(1)
+# space: O(1)
+def satisfiedWordleRule(word, green, yellow, gray):
+    not_this_word = False
+    for char_index in range(len(word)):
+        for y in yellow.keys():
+            if y not in word:
+                not_this_word = True
+                break
+        if not_this_word:
+            break
+        if word[char_index] in gray:
+            not_this_word = True
+            break
+        if word[char_index] in yellow.keys():
+            for yellow_num in yellow[word[char_index]]:
+                if char_index == yellow_num:
+                    not_this_word = True
+                    break
+            if not_this_word:
+                break
+        if char_index in green.keys() and word[char_index] != green[char_index]:
+            not_this_word = True
+            break
+    return not not_this_word
+
+
+def wordleSet(green, yellow, gray):
     res = []
     with open("test_data/wordle.txt", 'r', encoding="utf-8") as f:
         for word in f.readlines():
             word = word.replace("\n", "")
-            not_this_word = False
-            for char_index in range(len(word)):
-                for y in yellow.keys():
-                    if y not in word:
-                        not_this_word = True
-                        break
-                if not_this_word: 
-                    break
-                if word[char_index] in gray:
-                    not_this_word = True
-                    break
-                if word[char_index] in yellow.keys():
-                    for yellow_num in yellow[word[char_index]]:
-                        if char_index==yellow_num:
-                            not_this_word = True
-                            break
-                    if not_this_word:
-                        break
-                if char_index in green.keys() and word[char_index] != green[char_index]:
-                    not_this_word = True
-                    break
-            if not not_this_word:
+            if satisfiedWordleRule(word, green, yellow, gray):
                 # res += 1
                 res.append(word)
+    return res
+
+# Exercise 9 - Wordle Set
+
+
+def exercise9(green, yellow, gray):
+    res = wordleSet(green, yellow, gray)
     return len(res)
 
 # Exercise 10 - One Step of Wordle
 
 
-def exercise10(green, yellow, gray):
-    return None
+def exercise10(green: dict, yellow: dict, gray: set):
+    words = wordleSet(green, yellow, gray)
+    score = dict()
+    for wrong_word_index in range(len(words)):
+        wrong_word = words[wrong_word_index]
+        score[wrong_word] = 0
+        for correct_word in words:
+            if correct_word == words[wrong_word_index]:
+                # iterate all word in words except the assumed wrong word
+                continue
+            # Rebuild  wordle rules
+            green.clear()
+            yellow.clear()
+            gray.clear()
+            # This for-loop cost O(1) because length of word always be 5
+            for word_char_index in range(len(wrong_word)):
+                if wrong_word[word_char_index] == correct_word[word_char_index]:
+                    # Add green rule
+                    green[word_char_index] = correct_word[word_char_index]
+                else:
+                    if wrong_word[word_char_index] in correct_word:
+                        # Letter in wrong position but exist, set yellow rule
+                        if wrong_word[word_char_index] in yellow.keys():
+                            yellow[wrong_word[word_char_index]].append(
+                                word_char_index)
+                        else:
+                            yellow[wrong_word[word_char_index]] = [
+                                word_char_index, ]
+                    else:
+                        # Letter not exist, set gray rule
+                        gray.add(wrong_word[word_char_index])
 
+            # Calculating wrong_word score
+            for word in words:
+                if satisfiedWordleRule(word, green, yellow, gray):
+                    if wrong_word not in score.keys():
+                        score[wrong_word] = 1
+                    else:
+                        score[wrong_word] += 1
 
-### debug start ###
-print(exercise7(5, 2), "needed False")
-print(exercise7(1.15, 2), "needed False")
-### debug end ###
+    sorted_score = sorted(score.keys(), key=lambda key: score[key])
+    res = set({sorted_score[0]})
+    for i in sorted_score:
+        if not score[i] > score[sorted_score[0]]:
+            res.add(i)
+
+    return res
